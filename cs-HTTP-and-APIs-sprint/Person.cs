3f9dd637f5c98d18e5d4c513d61dd437e05735b1
Lili <lili.berenyi@gmail.com>
2024-12-16 14:10:29 +0000

    Initial commit

---
+++ cs-HTTP-and-APIs-sprint/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace cs_HTTP_and_APIs_sprint;
+
+internal class Person(string firstName, string lastName, string email, string phoneNumber, DateTime birthday, string gender)
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    [JsonPropertyName("firstname")]
+    public string FirstName { get; set; } = firstName;
+    [JsonPropertyName("lastname")]
+    public string LastName { get; set; } = lastName;
+    [JsonPropertyName("email")]
+    public string Email { get; set; } = email;
+    [JsonPropertyName("phone")]
+    public string PhoneNumber { get; set; } = phoneNumber;
+    [JsonPropertyName("birthday")]
+    public DateTime Birthday { get; set; } = birthday;
+    public string Gender { get; set; } = gender;
+}
---
+++ cs-HTTP-and-APIs-sprint/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace cs_HTTP_and_APIs_sprint;
+
+internal class Response(string status, int code, List<Person> persons)
+{
+    public string Status { get; set; } = status;
+    public int Code { get; set; } = code;
+    public List<Person> Persons { get; set; } = persons;
+}
